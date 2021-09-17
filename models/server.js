cdb29e7a9f1c2eb9ce42f4c2c8cee2ce0d1cc6c1
matisantillan11 <matias1.santillan@gmail.com>
2021-09-17 14:30:50 -0300

    creating model and route for person

--- models/server.js
+++ models/server.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const person = require('../routes/person')
 
 class Server {
 
     constructor(){
         this.app = express()
         this.port = process.env.PORT || 8080
-
+        this.personRoute = '/api/persons'
+        
         this.middlewares()
+        this.routes()
     }
 
 
     middlewares(){
         this.app.use(express.json())
         this.app.use(cors())
+    }
+
+    routes(){
+        this.app.use(this.personRoute, person)
     }
 
     listen(){
